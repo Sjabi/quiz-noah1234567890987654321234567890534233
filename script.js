12d120a968b66e3eeb90ccef5faf53c1e24b920e
[200~noah1234567890987654321234567890534233~ <noah.megroedt@student.sjabi.be>
2026-03-20 11:04:03 +0100

    implement quiz functionality with scoring and answer feedback

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ let vragen = [
 
 // ===== VARIABELEN =====
 let huidigeVraag = 0;
+let score = 0;
 
 
 // ===== START KNOP =====
@@ -30,26 +31,46 @@ function toonVraag() {
     let antwoordenDiv = document.getElementById("antwoorden");
 
     // TODO: vraag tonen
+    vraagElement.innerText = vragen[huidigeVraag].vraag;
     // TODO: antwoorden genereren (buttons!)
+    antwoordenDiv.innerHTML = "";
+
+    for (let i = 0; i < vragen[huidigeVraag].antwoorden.length; i++) {
+        let antwoordButton = document.createElement("button");
+        antwoordButton.innerText = vragen[huidigeVraag].antwoorden[i];
+        antwoordButton.onclick = function () {
+            controleerAntwoord(i);
+        };
+        antwoordenDiv.appendChild(antwoordButton);
+    }
 
 }
 
 
 // ===== ANTWOORD CONTROLEREN =====
 function controleerAntwoord(index) {
-
+    
     let feedback = document.getElementById("feedback");
-
     // TODO: controleer of antwoord juist is
+    if (index === vragen[huidigeVraag].correct) {
+        feedback.innerText = "Correct!";
+        feedback.style.color = "green";
+        score++;
+        document.getElementById("score2").innerText = score;        
+    } else {
+        feedback.innerText = "Fout!";
+        feedback.style.color = "red";
+    }
     // TODO: feedback tonen
-
+    
     document.getElementById("volgende").disabled = false;
+    
 }
 
 
 // ===== VOLGENDE VRAAG =====
 document.getElementById("volgende").onclick = function () {
-
+    feedback.innerText = "";
     huidigeVraag++;
 
     if (huidigeVraag >= vragen.length) {
@@ -58,4 +79,6 @@ document.getElementById("volgende").onclick = function () {
     }
 
     // TODO: nieuwe vraag tonen
+    toonVraag();
+    document.getElementById("button").disabled = false;
 };
